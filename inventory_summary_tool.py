ef0dd2949c7f3d55f1be0d2a1fe9a748761b07a5
Sanolar <Sannifaizolayotunde@gmail.com>
2025-06-03 13:35:09 -0400

    Added more to the existing products in the inventory.

--- inventory_summary_tool.py
+++ inventory_summary_tool.py
@@ -5,6 +5,13 @@ products = [
     {"name": "Apples", "qty": 12, "price": 1.5},
     {"name": "Bananas", "qty": 2, "price": 0.5},
     {"name": "Oranges", "qty": 0, "price": 0.8},
+    {"name": "Grapes", "qty": 20, "price": 2.0},
+    {"name": "Pineapples", "qty": 3, "price": 3.0},
+    {"name": "Mangoes", "qty": 8, "price": 1.2},
+    {"name": "Strawberries", "qty": 15, "price": 2.5},
+    {"name": "Blueberries", "qty": 1, "price": 4.0},
+    {"name": "Watermelons", "qty": 6, "price": 5.0},
+    {"name": "Peaches", "qty": 10, "price": 1.8}
 ]
 
 LOW_STOCK_THRESHOLD = 5
